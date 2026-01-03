d92302d5e3ecaf1b84b34432b04c9ff038e8cb9e
Dusko Vokic <vokic.dusko@gmail.com>
2026-01-03 17:04:28 +0100

    chore: update quiz title

--- src/components/header/HeaderTitle.jsx
+++ src/components/header/HeaderTitle.jsx
@@ -1,7 +1,7 @@
 function HeaderTitle() {
   return (
     <>
-      <h1 className="header-title">Quiz App</h1>
+      <h1 className="header-title">Quiz App Engine</h1>
       <p className="header-subtitle">
         Test your knowledge, beat your best score
       </p>
